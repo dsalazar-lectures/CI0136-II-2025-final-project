acad3152c0facba96e3a7cfc5bebf82989e8d170
Alexa <alexa.alpizar@ucr.ac.cr>
2025-09-27 16:12:51 -0600

    Method get_user_by_username implementation.

--- src/Infrastructure/User/UserRepository.py
+++ src/Infrastructure/User/UserRepository.py
@@ -3,9 +3,10 @@ from src.Model.User.User import User
 from src.Database.User.UserSchema import UserSchema # Import the UserSchema for database
 from src.Application.DTOs.UserDTO import UserDTO # Import the UserDTO for data transfer
 
+
 class UserRepository():
-    def __init__(self, database_connection): # Assumming db_connection
-        self.user_schema = UserSchema(database_connection)
+    def __init__(self, db_connection): # Assumming db_connection
+        self.user_schema = UserSchema(db_connection)
 
     def user_exists(self, username, email):
         # TODO: Check if user exists in the database
@@ -16,7 +17,7 @@ class UserRepository():
 
         if self.user_exists(user_dto.username, user_dto.email):
             raise ValueError("User with this username or email already exists")
-        
+
         # Convert DTO to JSON (dictionary)
         user_data = {
             "username": user_dto.username,
@@ -36,6 +37,14 @@ class UserRepository():
             )
         return None
 
-
     def get_user_by_username(self, username):
-        pass
+        db_user = self.user_schema.get_user_by_username(username)
+
+        if db_user:
+            return User(
+                id=db_user['id'],
+                username=db_user['username'],
+                email=db_user['email'],
+                role=db_user['role']
+            )
+        return None
