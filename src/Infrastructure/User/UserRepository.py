4ab014dde9993087cff9cbe134cf56defc634460
Alexa <alexa.alpizar@ucr.ac.cr>
2025-09-27 16:05:30 -0600

    Constructor added. Initializing UserSchema.

--- src/Infrastructure/User/UserRepository.py
+++ src/Infrastructure/User/UserRepository.py
@@ -4,8 +4,8 @@ from src.Database.User.UserSchema import UserSchema # Import the UserSchema for 
 from src.Application.DTOs.UserDTO import UserDTO # Import the UserDTO for data transfer
 
 class UserRepository():
-    def __init__(self, db_connection): # Assumming db_connection
-        self.user_schema = UserSchema(db_connection)
+    def __init__(self, database_connection): # Assumming db_connection
+        self.user_schema = UserSchema(database_connection)
 
     def user_exists(self, username, email):
         # TODO: Check if user exists in the database
