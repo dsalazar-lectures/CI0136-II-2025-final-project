94aa096b8ec68335bbc41a6eeaac1c62f0cf42e6
Alexa <alexa.alpizar@ucr.ac.cr>
2025-09-28 09:05:34 -0600

    In memory storage instead database

--- src/Infrastructure/User/UserRepository.py
+++ src/Infrastructure/User/UserRepository.py
@@ -1,47 +1,38 @@
-import json # For handling JSON data
 from src.Model.User.User import User
-from src.Database.User.UserSchema import UserSchema # Import the UserSchema for database
 from src.Application.DTOs.UserDTO import UserDTO # Import the UserDTO for data transfer
-
+from src.Databe.User.UserCSV import UserCSV # Import the UserCSV
 
 class UserRepository():
-    def __init__(self, db_connection): # Assumming db_connection
-        self.user_schema = UserSchema(db_connection)
+    def __init__(self, csv_file_path="users.csv"):
+        self.user_csv = UserCSV(csv_file_path)  # Delegate to UserCSV
 
     def user_exists(self, username, email):
         # TODO: Check if user exists in the database
-        return self.user_schema.user_exists(username, email) # Assuming user_schema has method user_exists
+        return self.user_csv.user_exists(username, email) # Delegate to UserCSV
 
     def create_user(self, user_dto):
         # TODO: Create user in the database
 
         if self.user_exists(user_dto.username, user_dto.email):
-            raise ValueError("User with this username or email already exists")
+            return None, "User already exists"
 
-        # Convert DTO to JSON (dictionary)
-        user_data = {
-            "username": user_dto.username,
-            "password": user_dto.password,
-            "email": user_dto.email,
-            "role": user_dto.role
-        }
-
-        created_user = self.user_schema.create_user(user_data) # Call create_user method from UserSchema
-
-        if created_user:
-            return User(
-                id=created_user['id'],
-                username=created_user['username'],
-                email=created_user['email'],
-                role=created_user['role']
+        created_user_data = self.user_csv.create_user(user_dto)
+        if created_user_data:
+            user_entity = User(
+                id=created_user_data['id'],
+                username=created_user_data['username'],
+                # password = created_user_data['password'],  # Password is already hashed in UserApplicationService
+                email=created_user_data['email'],
+                role=created_user_data['role']
             )
-        return None
+            return user_entity, "User created successfully"
+        return None, "Failed to create user"
 
     def get_user_by_username(self, username):
-        db_user = self.user_schema.get_user_by_username(username)
+        db_user = self.user_csv.get_user_by_username(username)
 
         if db_user:
-            return User(
+            return UserDTO(
                 id=db_user['id'],
                 username=db_user['username'],
                 email=db_user['email'],
