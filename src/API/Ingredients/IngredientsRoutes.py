27259d5a49206d878a2ad7aa407a5e097297e453
eduardo.sancho <eduardo.sancho@ucr.ac.cr>
2025-09-30 01:24:18 -0600

    implemented option to get ingredient by name

--- src/API/Ingredients/IngredientsRoutes.py
+++ src/API/Ingredients/IngredientsRoutes.py
@@ -10,9 +10,17 @@ def get_all_ingredients():
     return jsonify([ingredient.to_json() for ingredient in ingredients])
 
 @ingredients_bp.route("/ingredients/<int:ingredient_id>", methods=["GET"])
-def get_ingredient(ingredient_id):
+def get_ingredient_by_id(ingredient_id):
     """Get a specific ingredient by its ID."""
     ingredient = ingredient_service.get_ingredient_by_id(ingredient_id)
     if not ingredient:
+        return jsonify({"error": "Ingrediente no encontrado"}), 404
+    return jsonify(ingredient.to_json())
+
+@ingredients_bp.route("/ingredients/<string:ingredient_name>", methods=["GET"])
+def get_ingredient_name(ingredient_name):
+    """Get a specific ingredient by its name."""
+    ingredient = ingredient_service.get_ingredient_by_name(ingredient_name)
+    if ingredient == None:
         return jsonify({"error": "Ingrediente no encontrado"}), 404
     return jsonify(ingredient.to_json())
--- src/Application/Ingredients/IngredientUseCase.py
+++ src/Application/Ingredients/IngredientUseCase.py
@@ -13,6 +13,10 @@ class IngredientUseCase:
     def get_ingredient_by_id(self, ingredient_id: int) -> Optional[Ingredient]:
         """Get a specific ingredient by its ID."""
         return self.repository.get_by_id(ingredient_id)
+    
+    def get_ingredient_by_name(self, ingredient_name: str) -> Optional[Ingredient]:
+        """Get a specific ingredient by its name."""
+        return self.repository.get_by_name(ingredient_name)
 
 # Create a singleton instance to be imported by the routes
 ingredient_service = IngredientUseCase()
