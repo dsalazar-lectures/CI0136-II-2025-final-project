900ce399e9945e0d1ff7c8853320b3213e6e832d
jimeeena <jimerivera0210@gmail.com>
2025-10-21 21:13:00 -0600

    add granular methods to IValidationService (validate_username/email/password)

--- src/Application/Interfaces/IValidationService.py
+++ src/Application/Interfaces/IValidationService.py
@@ -7,5 +7,17 @@ class IValidationService(ABC):
         pass
 
     @abstractmethod
+    def validate_username(self, username):
+        pass
+
+    @abstractmethod
+    def validate_email(self, email):
+        pass
+
+    @abstractmethod
+    def validate_password(self, password):
+        pass
+
+    @abstractmethod
     def validate_userdata(self, username, password, email):
         pass
