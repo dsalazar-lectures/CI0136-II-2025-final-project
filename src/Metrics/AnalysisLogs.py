fb9a4a8618681ffbab5a762d992ce58d60514619
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-10-23 21:05:53 -0600

    Add Most Search Recipes Plot

--- src/Metrics/AnalysisLogs.py
+++ src/Metrics/AnalysisLogs.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Any, Tuple, Optional
 import pandas as pd
 import streamlit as st
+from src.Application.Recipes import IRecipeRepository
 
 #--------------------------------------------------------------------------------
 
@@ -17,10 +18,32 @@ def top_users_least_active(df_list, start_date, end_date):
 def top_users_most_active(df_list, start_date, end_date):
     pass
 
+def top_most_search_recipes(df_list, start_date, end_date):
+    df = None
+    for item in df_list:
+        if item['name'] == 'Search recipes':
+            df = item["data"]
+            break
+    
+    if df is None or df.empty:
+        st.text("No 'Search recipes' data found.")
+    else:
+        #st.dataframe(df)
+
+        counts = df['Id_Producto'].value_counts()
+        recipes_counts = {}
+        for recipe_id in df['Id_Producto'].unique():
+            recipe = IRecipeRepository.recipe_repository.get_by_id(recipe_id)
+            if recipe:
+                recipes_counts[recipe.to_dict()["name"]] = counts[recipe_id]
+                
+        #st.text(recipes_counts)
+        st.bar_chart(recipes_counts)
 
 analysis_registry = {
     "Least Active Users": top_users_least_active,
     "Most Active Users": top_users_most_active,
+    "Most Searched Recipes": top_most_search_recipes,
 }
 
 def show_analysis_logs(df_list, func, start_date, end_date):
