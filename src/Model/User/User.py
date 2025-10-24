265ee6a9254528dd41a406e5da463114fa9acb69
jimeeena <jimerivera0210@gmail.com>
2025-10-24 10:28:52 -0600

    Include token and key in User.to_dict

--- src/Model/User/User.py
+++ src/Model/User/User.py
@@ -1,10 +1,12 @@
 class User:
-    def __init__(self, id, username, password, email, role="user"):
+    def __init__(self, id, username, password, email, role="user", key=None, token=None):
         self.id = id
         self.username = username
         self.password = password
         self.email = email
         self.role = role
+        self.key = key
+        self.token = token
 
     def to_dict(self):
         # Turn user object into a dictionary
@@ -13,5 +15,7 @@ class User:
             'username': self.username,
             'password': self.password,
             'email': self.email,
-            'role': self.role
+            'role': self.role,
+            'key': self.key,
+            'token': self.token
         }
