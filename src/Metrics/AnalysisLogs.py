b6c5f20ecf10488f76ae4064252f4eccee22da98
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-10-24 14:45:30 -0600

    Add Most Search Recipes to Analysis Logs class

--- src/Metrics/AnalysisLogs.py
+++ src/Metrics/AnalysisLogs.py
@@ -19,33 +19,35 @@ class AnalysisLogs:
     def top_users_most_active(df_list, start_date, end_date):
         pass
 
+    @staticmethod
+    def top_most_search_recipes(df_list, start_date, end_date):
+        if df_list is None or len(df_list) == 0:
+            st.text(MSG_NO_DATA_OR_FUNC)
+        else:
+            df = None
+            for item in df_list:
+                if item['name'] == 'Search recipes':
+                    df = item["data"]
+                    break
+            
+            if df is None or df.empty:
+                st.text("No 'Search recipes' data found.")
+            else:
+                counts = df['Id_Producto'].value_counts().head(10)
+                recipes_counts = {}
+                for recipe_id in df['Id_Producto'].unique():
+                    recipe = IRecipeRepository.recipe_repository.get_by_id(recipe_id)
+                    if recipe:
+                        recipes_counts[recipe.to_dict()["name"]] = counts[recipe_id]
+                        
+                st.bar_chart(recipes_counts)
 
     analysis_registry: Dict[str, Callable[..., None]] = {
         "Least Active Users": top_users_least_active,
         "Most Active Users": top_users_most_active,
+        "Most Searched Recipes": top_most_search_recipes,
     }
 
-def top_most_search_recipes(df_list, start_date, end_date):
-    if df_list is None or len(df_list) == 0:
-        st.text(MSG_NO_DATA_OR_FUNC)
-    else:
-        df = None
-        for item in df_list:
-            if item['name'] == 'Search recipes':
-                df = item["data"]
-                break
-        
-        if df is None or df.empty:
-            st.text("No 'Search recipes' data found.")
-        else:
-            counts = df['Id_Producto'].value_counts().head(10)
-            recipes_counts = {}
-            for recipe_id in df['Id_Producto'].unique():
-                recipe = IRecipeRepository.recipe_repository.get_by_id(recipe_id)
-                if recipe:
-                    recipes_counts[recipe.to_dict()["name"]] = counts[recipe_id]
-                    
-            st.bar_chart(recipes_counts)
 
     @staticmethod
     def show_analysis_logs(df_list, func, start_date, end_date):
