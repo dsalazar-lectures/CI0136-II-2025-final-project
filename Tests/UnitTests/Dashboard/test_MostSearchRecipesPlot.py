fccfdc549f3cdeae32b04763a1d604aeb16770ea
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-10-24 16:02:28 -0600

    Moved test folder to root

--- Tests/UnitTests/Dashboard/test_MostSearchRecipesPlot.py
+++ Tests/UnitTests/Dashboard/test_MostSearchRecipesPlot.py
@@ -1,26 +1,24 @@
+# import sys
+# from pathlib import Path
+# sys.path.append(str(Path(__file__).resolve().parents[2] / "src"))
+
 import unittest
 from unittest.mock import patch, MagicMock
-from src.Metrics.AnalysisLogs import top_most_search_recipes
+from src.Metrics.AnalysisLogs import AnalysisLogs
 
 class MostSearchRecipesPlotTest(unittest.TestCase):
 
     @patch("src.Metrics.AnalysisLogs.st.text", new_callable=MagicMock)
     def test_top_most_search_recipes_with_no_data(self, mock_st_text):
         fake_df_list = []
-        top_most_search_recipes(fake_df_list, None, None)
+        AnalysisLogs.top_most_search_recipes(fake_df_list, None, None)
         mock_st_text.assert_called_once_with("No data or metrics available to display.")
 
     @patch("src.Metrics.AnalysisLogs.st.text", new_callable=MagicMock)
     def test_top_most_search_recipes_with_empty_data(self, mock_st_text):
         fake_df_list = [{'name': 'Search recipes', 'data': MagicMock(empty=True)}]
-        top_most_search_recipes(fake_df_list, None, None)
+        AnalysisLogs.top_most_search_recipes(fake_df_list, None, None)
         mock_st_text.assert_called_once_with("No 'Search recipes' data found.")
-        
-    # @patch("src.Metrics.AnalysisLogs.st.bar_chart", new_callable=MagicMock)
-    # def test_top_most_search_recipes_with_good_data(self, mock_st_chart):
-    #     fake_df_list = [{'name': 'Search recipes', 'data': MagicMock(empty=False)}]
-    #     top_most_search_recipes(fake_df_list, None, None)
-    #     mock_st_chart.assert_called_once_with("No 'Search recipes' data found.")
         
 if __name__ == '__main__':
     unittest.main()
