bd6688e0a388bdb42c141467d57f8a2d361f4d5c
eduardo.sancho <eduardo.sancho@ucr.ac.cr>
2025-10-25 14:54:18 -0600

    added create, update and delete methods to use case class

--- src/Application/Ingredients/IngredientUseCase.py
+++ src/Application/Ingredients/IngredientUseCase.py
@@ -25,6 +25,26 @@ class IngredientUseCase:
     def create_ingredient(self, name, categories, substitutes, components, recipe_count = 0):
         """Create a new ingredient"""
         return self.repository.create_ingredient(name, categories, substitutes, components, recipe_count)
+    
+    def add_recipe(self, ingredient_id: int):
+        """Add a recipe to an ingredient's recipe count"""
+        return self.repository.add_recipe(ingredient_id)
+    
+    def update_categories(self, ingredient_id: int, categories):
+        """Update an ingredient's categories"""
+        return self.repository.update_ingredient_categories(ingredient_id, categories)
+    
+    def update_substitutes(self, ingredient_id: int, substitutes):
+        """Update an ingredient's substitutes"""
+        return self.repository.update_ingredient_substitutes(ingredient_id, substitutes)
+    
+    def update_components(self, ingredient_id: int, components):
+        """Update an ingredient's components"""
+        return self.repository.update_ingredient_components(ingredient_id, components)
+    
+    def delete_ingredient(self, ingredient_id: int):
+        """Delete an ingredient from the ingredient dictionary"""
+        return self.repository.delete_ingredient(ingredient_id)
 
 
 # Create a singleton instance to be imported by the routes
