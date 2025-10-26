f632bef88a6bca2defa51b204b12ea2ee9410113
pauqiu <zhpaulette@gmail.com>
2025-10-26 15:00:54 -0600

    Mock classes of services

---
+++ tests/Mocks/Users/mock_user_repo.py
@@ -0,0 +1,44 @@
+from src.Application.Interfaces.IUserRepository import IUserRepository
+from src.Application.DTOs.UserDTO import UserDTO
+from tests.Mocks.Users.mock_user import MockUser
+
+
+class MockUserRepository(IUserRepository):
+    def __init__(self, users=None):
+        self._users = users or []
+        self._next_id = 1
+
+    def user_exists(self, username, email):
+        return any(
+            user.username == username or user.email == email for user in self._users
+        )
+
+    def create_user(self, user_dto):
+        if self.user_exists(user_dto.username, user_dto.email):
+            return None, "User already exists", 400
+
+        created_dto = UserDTO(
+            id=self._next_id,
+            username=user_dto.username,
+            password=user_dto.password,
+            email=user_dto.email,
+            role=user_dto.role,
+        )
+
+        mock_user = MockUser(
+            id=self._next_id,
+            username=user_dto.username,
+            password=user_dto.password,
+            email=user_dto.email,
+            role=user_dto.role,
+        )
+        self._users.append(mock_user)
+        self._next_id += 1
+
+        return created_dto, "User created successfully", 201
+
+    def get_user_by_username(self, username):
+        for user in self._users:
+            if user.username == username:
+                return user
+        return None
