2e847afca95b934c92dcd72da6844acba8a68d74
Katherine Acosta <katherine.acostabarquero@ucr.ac.cr>
2025-10-25 19:22:36 -0600

    Add get_random_recipe_by_category function to retrieve a random recipe from a specified category

--- src/Application/Recipes/recipe_service.py
+++ src/Application/Recipes/recipe_service.py
@@ -1,15 +1,20 @@
+import random
 from src.Infrastructure.Recipes.CSVRecipeRepository import CSVRecipeRepository
 
 recipe_repository = CSVRecipeRepository()
 
+
 def get_all_recipes():
     return recipe_repository.get_all()
+
 
 def get_recipe_by_id(recipe_id):
     return recipe_repository.get_by_id(recipe_id)
 
+
 def get_recipes_by_ingredient(ingredient):
     return recipe_repository.find_by_ingredient(ingredient)
+
 
 def create_recipe(recipe_data, username):
     recipe_data["author"] = username
@@ -17,13 +22,16 @@ def create_recipe(recipe_data, username):
         return -1
     return recipe_repository.add_recipe(recipe_data)
 
+
 def delete_recipe(recipe_id, username):
     return recipe_repository.delete_if_owned(recipe_id, username)
+
 
 def update_recipe(recipe_id, updates, username):
     if not validate_data(updates):
         return -1
     return recipe_repository.update_if_owned(recipe_id, username, updates)
+
 
 def validate_data(recipe_data) -> bool:
     for key, value in recipe_data.items():
@@ -42,6 +50,10 @@ def validate_data(recipe_data) -> bool:
                 return False
     return True
 
-def get_recipes_by_category(category):
-    return recipe_repository.find_by_category(category)
-    return True
+
+def get_random_recipe_by_category(category):
+    recipes = recipe_repository.find_by_category(category)
+    if not recipes:
+        return None
+
+    return random.choice(recipes)
