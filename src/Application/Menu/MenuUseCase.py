1641142e42b9788e109cd93ea066f7d4f57c134c
Aldo Cascante <cascante.aldo@gmail.com>
2025-10-26 09:36:43 -0600

    check for basic email address formating

--- src/Application/Menu/MenuUseCase.py
+++ src/Application/Menu/MenuUseCase.py
@@ -1,9 +1,13 @@
+import re
 from src.Application.Menu.MenuPdfAdapter import MenuPdfAdapter
 from src.Services.EmailService import sendMenu
 
 
 def emailPdf(recipeList, recipientEmail):
 
+    if not re.match("[^@]+@[^@]+\.[^@]+", recipientEmail):
+        return "400"
+    
     adapter = MenuPdfAdapter(recipeList)
     sendMenu(recipientEmail, adapter)
 
