15180cdcee1ac384264e930e200778588372c5c0
Carlos Obando <carlos.obandolopez@ucr.ac.cr>
2025-10-26 16:09:19 -0600

    Implement IMenuRepository interface and add generate_menus function for menu generation

---
+++ src/Application/Menu/menu_service.py
@@ -0,0 +1,22 @@
+from typing import Any, List, Dict
+
+
+def generate_menus(recipes, count) -> List[Dict]:
+    # Obteraining number of recipes
+    n_recipes = len(recipes)
+
+    # If there are no recipes, we simply return an empty list
+    if n_recipes == 0:
+        return []
+
+    # Generate menus
+    menus = []
+    for i in range(1, count + 1):
+        recipe = recipes[(i - 1) % n_recipes]
+        menus.append(
+            {
+                "menu": f"Menú #{i}",
+                "recipe": recipe.to_dict(),
+            }
+        )
+    return menus
