630955618437e432d8338b43c04b96cad71cfc3c
Katherine Acosta <katherine.acostabarquero@ucr.ac.cr>
2025-10-26 14:48:57 -0600

    Rename blueprint variable for menu routes for consistency

--- src/API/Menu/menuRoutes.py
+++ src/API/Menu/menuRoutes.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify, request
 from src.Application.Recipes import recipe_service
 
-recipes_bp = Blueprint("menu", __name__)
+menu_bp = Blueprint("menu", __name__)
 
 
-@recipes_bp.route("/menu", methods=["GET"])
+@menu_bp.route("/menu", methods=["GET"])
 def get_menu():
     category = request.args.get("category")
     recipe = recipe_service.get_random_recipe_by_category(category)
