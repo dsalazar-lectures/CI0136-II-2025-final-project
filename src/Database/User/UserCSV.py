2c8b7b0fc1b056a27a7e49a251b4525579adbc36
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa.local>
2025-10-25 21:30:53 -0600

    update_email method implemented.

--- src/Database/User/UserCSV.py
+++ src/Database/User/UserCSV.py
@@ -76,3 +76,24 @@ class UserCSV:
             return True, "Password updated successfully", 200
         
         return False, "Failed to update password", 400
+    
+    def update_email(self, username, new_email):
+        updated = False
+        rows = []
+
+        with open(self.file_path, 'r', newline='') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['username'] == username:
+                    row['email'] = new_email
+                    updated = True
+                rows.append(row)
+
+        if updated:
+            with open(self.file_path, 'w', newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=['id', 'username', 'password', 'email', 'role'])
+                writer.writeheader()
+                writer.writerows(rows)
+            return True, "Email updated successfully", 200
+
+        return False, "User not found", 404
