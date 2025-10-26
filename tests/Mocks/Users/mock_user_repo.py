b9a444752ac3a38cbc2a886df14358b92e818ab3
pauqiu <zhpaulette@gmail.com>
2025-10-26 17:57:00 -0600

    Mock of get_user_by_token and update_token

--- tests/Mocks/Users/mock_user_repo.py
+++ tests/Mocks/Users/mock_user_repo.py
@@ -44,7 +44,13 @@ class MockUserRepository(IUserRepository):
         return None
 
     def get_user_by_token(self, token):
-        pass
+        for user in self._users:
+            if user.token == token:
+                return user
+        return None
 
     def update_user_token(self, username, token, key) -> bool:
-        return self.user_csv.update_user_token(username, token, key)
+        for user in self._users:
+            if user.username == username:
+                user.token = token
+        return False
