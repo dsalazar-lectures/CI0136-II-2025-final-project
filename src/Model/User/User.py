61ee4d7c56b99a94183394cfa82bea3b443a1d99
pauqiu <zhpaulette@gmail.com>
2025-10-26 01:02:09 -0600

    adding key and token to the model

--- src/Model/User/User.py
+++ src/Model/User/User.py
@@ -1,17 +1,21 @@
 class User:
-    def __init__(self, id, username, password, email, role="user"):
+    def __init__(self, id, username, password, email, role="user", key=None, token=None):
         self.id = id
         self.username = username
         self.password = password
         self.email = email
         self.role = role
+        self.key = key
+        self.token = token
 
     def to_dict(self):
         # Turn user object into a dictionary
         return {
-            "id": self.id,
-            "username": self.username,
-            "password": self.password,
-            "email": self.email,
-            "role": self.role,
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'email': self.email,
+            'role': self.role,
+            'key': self.key,
+            'token': self.token
         }
