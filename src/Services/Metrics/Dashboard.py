6e7e11f87a7e8533d4689397bf89456dd41bf9bd
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-10-27 17:14:20 -0600

    Update Dashboard to support src importations
    Streamlib executes this dashboard without the src route, which cause problems if anything is imported with the route src/

--- src/Services/Metrics/Dashboard.py
+++ src/Services/Metrics/Dashboard.py
@@ -1,3 +1,8 @@
+import sys, os
+
+ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../.."))
+if ROOT not in sys.path:
+    sys.path.append(ROOT)
 import streamlit as st
 from pathlib import Path
 from typing import Iterable
