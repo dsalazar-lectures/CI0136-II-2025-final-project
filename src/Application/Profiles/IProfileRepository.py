a0d5c81c688e0207e52b3834bf4430e67dd130af
AlexSM15 <alexanderwangwu@gmail.com>
2025-10-27 16:47:28 -0600

    Add methods for use in profile

--- src/Application/Profiles/IProfileRepository.py
+++ src/Application/Profiles/IProfileRepository.py
@@ -1,7 +1,16 @@
 from abc import ABC, abstractmethod
-
+from typing import List, Optional
+from src.Model.Profiles.Profiles import Profile
 
 class IProfileRepository(ABC):
     @abstractmethod
     def create_profile(self, user_id):
+        pass
+
+    @abstractmethod
+    def get_profile(self, user_id) -> Optional[Profile]:
+        pass
+
+    @abstractmethod
+    def update_favorite_foods(self, user_id, favorites) -> Optional[Profile]:
         pass
--- src/Infrastructure/Profiles/ProfileRepository.py
+++ src/Infrastructure/Profiles/ProfileRepository.py
@@ -1,7 +1,7 @@
 from src.Application.Profiles.IProfileRepository import IProfileRepository
 from src.Model.Profiles.Profiles import Profile
 from src.Database.Profiles.ProfileCSV import ProfileCSV
-
+from typing import Optional
 
 class ProfileRepository(IProfileRepository):
     def __init__(self, csv_file_path="profiles.csv"):
@@ -23,3 +23,25 @@ class ProfileRepository(IProfileRepository):
                 favorite_menus=created_profile_data["favorite_menus"],
             )
         return None
+
+    def get_profile(self, user_id) -> Optional[Profile]:
+        data = self.profile_database.get_profile(user_id)
+        if not data:
+            return None
+        return Profile (
+            user_id = int(data["user_id"]),
+            favorite_foods=data["favorite_foods"],
+            unfavorite_foods=data["unfavorite_foods"],
+            favorite_menus=data["favorite_menus"],
+        )
+    
+    def update_favorite_foods(self, user_id, favorites) -> Optional[Profile]:
+        updated = self.profile_database.update_fields(user_id, {"favorite_foods": favorites})
+        if not updated:
+            return None
+        return Profile(
+            user_id=int(updated["user_id"]),
+            favorite_foods=updated["favorite_foods"],
+            unfavorite_foods=updated["unfavorite_foods"],
+            favorite_menus=updated["favorite_menus"],
+        )
