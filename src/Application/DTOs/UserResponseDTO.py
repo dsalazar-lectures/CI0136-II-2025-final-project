60c3b10c8e26e7807ad5d62612f5e5e934eb3fdd
¨amber.villarreal¨ <amber.villarreal@ucr.ac.cr>
2025-10-26 18:36:41 -0600

    Add UserResponseDTO for user response handling  (without password) and update UserRepository to use it

---
+++ src/Application/DTOs/UserResponseDTO.py
@@ -0,0 +1,11 @@
+# src/Application/DTOs/UserResponseDTO.py
+class UserResponseDTO:
+    """DTO solo para responses, sin datos sensibles"""
+    def __init__(self, id, username, email, role="user"):
+        self.id = id
+        self.username = username
+        self.email = email
+        self.role = role
+
+    def to_dict(self):
+        return self.__dict__
