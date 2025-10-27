795ed8ea83f6dd345b9ff961fdea5e150095429c
¨amber.villarreal¨ <amber.villarreal@ucr.ac.cr>
2025-10-26 19:33:28 -0600

    Refactor ProfileApplicationService to allow dependency injection for the profile repository and add unit tests for create_profile method.

--- src/Application/Profiles/Services/ProfileApplicationService.py
+++ src/Application/Profiles/Services/ProfileApplicationService.py
@@ -1,11 +1,9 @@
-from src.Infrastructure.Profiles.ProfileRepository import (
-    ProfileRepository,
-)
+from src.Infrastructure.Profiles.ProfileRepository import ProfileRepository
 
 
 class ProfileApplicationService:
-    def __init__(self):
-        self.profile_repository = ProfileRepository()
+    def __init__(self, profile_repository=None):
+        self.profile_repository = profile_repository or ProfileRepository()
 
     def create_profile(self, user_id: int):
         return self.profile_repository.create_profile(user_id)
