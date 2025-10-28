d98ff37fe8bbdcdc09aa4fe694b38e4216bb20c5
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-10-27 20:15:43 -0600

    Revert "Revert "Merge pull request #80 from dsalazar-lectures/BBQ-Dashboard""
    This reverts commit 90162d922aa1b676f1bd276e46c4b9faf411cda8.

---
+++ tests/Unittests/Dashboard/test_GeneralLogs.py
@@ -0,0 +1,23 @@
+import unittest
+from pathlib import Path
+import sys
+
+sys.path.append(str(Path(__file__).resolve().parents[3] / "src"))
+sys.path.append(str(Path(__file__).resolve().parents[2] / "Mocks"))
+
+from src.Services.Metrics.GeneralLogs import GeneralLogs
+from tests.Mocks.Dashboard.Mock_GeneralLogs import MocksGeneralLogs
+
+
+class TestGeneralLogs(unittest.TestCase):
+    def test_show_logs_table_with_data(self):
+        df = MocksGeneralLogs.get_sample_df()
+        with MocksGeneralLogs.patch_st() as mock_st:
+            GeneralLogs.show_logs_table(df)
+            mock_st.dataframe.assert_called_once_with(df)
+
+    def test_show_logs_table_empty(self):
+        df = MocksGeneralLogs.get_empty_df()
+        with MocksGeneralLogs.patch_st() as mock_st:
+            GeneralLogs.show_logs_table(df)
+            mock_st.info.assert_called_once()
