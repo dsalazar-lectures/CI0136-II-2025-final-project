ca6ba565dbe0a743dabbea76d61abe4a302431a9
JamesLezcano <james.jar2@gmail.com>
2025-11-12 20:16:53 -0600

    Implement Singleton pattern in CustomLogger to prevent multiple logger instances across different modules

--- src/Shared/Logs/custom_logger.py
+++ src/Shared/Logs/custom_logger.py
@@ -1,17 +1,29 @@
 import logging
+import uuid
 from dataclasses import asdict
 from typing import Iterable, Optional
 from src.Shared.Logs.audit_event import AuditEvent
 
 
 class CustomLogger:
+    _instance = None
 
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super(CustomLogger, cls).__new__(cls)
+            cls._instance._instance_id = f"SID-{uuid.uuid4().hex[:8]}"
+            print(f"[INFO] Singleton CustomLogger inicializado con ID {cls._instance._instance_id}")
+        return cls._instance
+    
     def __init__(
         self,
         name: str = "AppLogger",
         handlers: Optional[Iterable[logging.Handler]] = None,
         level: int = logging.DEBUG,
     ) -> None:
+        if hasattr(self, "_initialized") and self._initialized:
+            return
+        
         self.logger = logging.getLogger(name)
         self.logger.setLevel(level)
         self.logger.propagate = False
