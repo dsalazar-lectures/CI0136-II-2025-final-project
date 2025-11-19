44af2b7563a91b63461ee0b88354380cf62a00fd
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-11-19 16:09:30 -0600

    Fix linting in app

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(profiles_bp, url_prefix="/api")
     app.register_blueprint(dashboard_bp, url_prefix="/api")
-    
+
     # Create logger singleton instance
     CustomLogger(handlers=build_handlers())
 
