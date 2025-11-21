d62a89211b2d034292b89a8d1d6648429026fe11
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa-2.local>
2025-11-21 15:36:10 -0600

    IUserRepository updated

--- src/Application/Interfaces/IUserRepository.py
+++ src/Application/Interfaces/IUserRepository.py
@@ -22,3 +22,11 @@ class IUserRepository(ABC):
     @abstractmethod
     def update_email(self, username: str, new_email: str):
         pass
+    
+    @abstractmethod
+    def get_user_by_email(self, email):
+        pass
+
+    @abstractmethod
+    def update_password_by_id(self, user_id: str, hashed_password: str):
+        pass
