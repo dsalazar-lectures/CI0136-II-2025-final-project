946101ec79549570ca9302f5f32860012e7b6308
Alexa Alpízar Mora <aalpizar@MacBook-Pro-de-Alexa-2.local>
2025-11-23 12:54:07 -0600

    Validate email format method added.

--- src/Application/User/Services/ValidationService.py
+++ src/Application/User/Services/ValidationService.py
@@ -51,3 +51,12 @@ class ValidationService(IValidationService):
             return False, "Password must contain at least one special character."
 
         return True, "Password format is valid."
+
+    @staticmethod
+    def validate_email_format(email: str):
+        
+        # Validates that the email has a correct format using a regex pattern.
+        pattern = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+        if re.match(pattern, email):
+            return True, "Valid email format"
+        return False, "Invalid email format. Example: user@example.com"
