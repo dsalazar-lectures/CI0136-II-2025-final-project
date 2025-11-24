368cdeb584e9063ea1d8c618bef8aba09d57d149
Arianna Cortes <ariannacortesr@gmail.com>
2025-11-23 18:08:05 -0600

    Added external recipe integration with TheMealDB API. API recipes are stored in a different csv for deletion management but undergoes the same filtering process as the system recipes. CSV is currently empty for demo purposes as it will be used to showcase the loading process.

--- src/Infrastructure/Recipes/CSVRecipeRepository.py
+++ src/Infrastructure/Recipes/CSVRecipeRepository.py
@@ -2,6 +2,7 @@ import csv
 from src.Database.Recipes.RecipesSchema import system_recipes, PATH, load_recipes
 from src.Model.Recipes.Recipes import Recipe
 from src.Application.Recipes.IRecipeRepository import IRecipeRepository
+from src.Database.Recipes.APIRecipesSchema import api_recipes, load_api_recipes
 
 # storage for user-created recipes separate from CSV system_recipes
 
@@ -9,20 +10,34 @@ from src.Application.Recipes.IRecipeRepository import IRecipeRepository
 class CSVRecipeRepository(IRecipeRepository):
     def get_all(self):
         load_recipes()
+        load_api_recipes()
+        all_recipes = system_recipes + api_recipes
+        return all_recipes
+    
+    def get_api_recipes():
+        load_api_recipes()
+        return api_recipes
+    
+    def get_system_recipes():
+        load_recipes()
         return system_recipes
 
     def get_by_id(self, recipe_id):
         load_recipes()
+        load_api_recipes()
+        all_recipes = system_recipes + api_recipes
         return next(
-            (recipe for recipe in system_recipes if recipe.id == recipe_id), None
+            (recipe for recipe in all_recipes if recipe.id == recipe_id), None
         )
 
     def find_by_ingredient(self, ingredient):
         load_recipes()
+        load_api_recipes()
+        all_recipes = system_recipes + api_recipes
         ingredient = ingredient.lower()
         return [
             recipe
-            for recipe in system_recipes
+            for recipe in all_recipes
             if any(ingredient in ing.lower() for ing in recipe.ingredients)
         ]
 
@@ -140,10 +155,12 @@ class CSVRecipeRepository(IRecipeRepository):
 
     def find_by_category(self, category):
         load_recipes()
+        load_api_recipes()
+        all_recipes = api_recipes + system_recipes
         category = category.lower()
         return [
             recipe
-            for recipe in system_recipes
+            for recipe in all_recipes
             if any(
                 category in str(recipe_category).lower()
                 for recipe_category in (recipe.categories)
