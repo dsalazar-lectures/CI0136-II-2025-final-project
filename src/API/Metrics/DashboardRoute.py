7f4ec84f31e519644455e977f1cd059b7fc228f8
Alexander Sanchez Zamora <alexander.sanchezzamora@ucr.ac.cr>
2025-11-28 20:42:01 -0600

    Update dashboard route to use default AuthService

--- src/API/Metrics/DashboardRoute.py
+++ src/API/Metrics/DashboardRoute.py
@@ -7,31 +7,11 @@ import socket
 import webbrowser
 from pathlib import Path
 import os
-from src.Application.User.Services.AuthorizationService import AuthorizationService
-from src.Application.User.Services.UserApplicationService import UserApplicationService
-from src.Application.User.Services.EncryptionService import EncryptionService
-from src.Application.User.Services.ValidationService import ValidationService
-from src.Application.User.Services.TokenService import TokenService
-from src.Infrastructure.User.UserRepository import UserRepository
-from src.Infrastructure.Profiles.ProfileRepository import ProfileRepository
-from src.Application.Profiles.Services.ProfileApplicationService import (
-    ProfileApplicationService,
-)
+from src.Application.User.Services.AuthorizationService import create_default_auth_service
 from src.Model.Profiles.Roles import Role
 
-user_repository = UserRepository()
-validation_service = ValidationService()
-encryption_service = EncryptionService()
-token_service = TokenService()
-profile_service = ProfileApplicationService(profile_repository=ProfileRepository())
-user_app_service = UserApplicationService(
-    user_repository=user_repository,
-    validation_service=validation_service,
-    encryption_service=encryption_service,
-    token_service=token_service,
-    profile_service=profile_service,
-)
-auth_service = AuthorizationService(user_app_service, profile_service)
+
+auth_service = create_default_auth_service()
 
 dashboard_bp = Blueprint("dashboard", __name__)
 
